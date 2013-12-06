7a80b663ae963cb28b6206736e059d7a578dc1c5
Colin Shields <N02433728@hawkmail.newpaltz.edu>
2013-12-06 15:59:35 -0500

    Changed output, Cleaned folder

--- overdraft.rb
+++ overdraft.rb
@@ -1,4 +1,5 @@
 class Overdraft
+  attr_reader :balance, :date, :expense, :type
   def initialize (balance, transaction)
     @balance = balance
     @date = transaction.date
--- tracker.rb
+++ tracker.rb
@@ -9,7 +9,13 @@ require_relative 'account'
 
 colin = Customer.new('Colin','Shields',Account.new(TransactionReader.get_transactions('transactions.csv')))
 colin.account.process_transactions
+puts colin.first_name + ' ' + colin.last_name
 puts "Ending Balance : #{sprintf('%.2f', colin.account.balance)}"
 puts "Total Deposited : #{sprintf('%.2f', colin.account.total_deposited)}"
 puts "Total Withdrawn : #{sprintf('%.2f', colin.account.total_withdrawn)}"
 puts "Total Overdraft Fees : #{sprintf('%.2f', colin.account.overdrafts.length * 20.0)}"
+puts
+puts 'Overdrafts (balance, expense, description, date'
+colin.account.overdrafts.each do |overdraft|
+  puts "#{sprintf('%.2f', overdraft.balance)}, #{overdraft.expense}, #{overdraft.type}, #{overdraft.date}"
+end
